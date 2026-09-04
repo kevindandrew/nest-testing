19bf1a2255b11c89caacffc1e23eef5901f666a0
kevindandrew <kevindandrew77@gmail.com>
2026-09-04 14:03:41 -0400

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { AppModule } from "./app.module";
-
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -13,6 +13,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle("Cats example")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("cats")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api-docs", app, documentFactory);
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>("PORT") ?? 3000;
